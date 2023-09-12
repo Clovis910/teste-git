0a1c4169f270524cdadb7f9ddf9af7092bd61341
Clovis910 <clovis910@hotmail.com>
2023-09-12 19:48:38 -0300

    Alteracao do Ola

--- OlaMundo.java
+++ OlaMundo.java
@@ -3,6 +3,6 @@ public class OlaMundo {
 
     public static void main(String[] args) {
         
-            System.out.println("Ola MUndo");
+            System.out.println("Ola Fulano");
     }
 }
