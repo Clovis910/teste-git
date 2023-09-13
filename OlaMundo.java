3e23d055fd8f836920e714552778398add9af7e5
Clovis910 <clovis910@hotmail.com>
2023-09-12 21:14:17 -0300

    funcionalidade2

--- OlaMundo.java
+++ OlaMundo.java
@@ -4,5 +4,7 @@ public class OlaMundo {
     public static void main(String[] args) {
         
             System.out.println("Ola Fulano");
+
+            System.out.println("funcionalidade 2");
     }
 }
